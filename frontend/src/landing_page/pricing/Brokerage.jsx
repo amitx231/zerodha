5067c5e592b4d177b2f7e028d7fcc56a53d3b78f
Amit Chauhan <amitchauhan822006@gmail.com>
2026-07-14 15:58:28 +0530

    Develop Pricing Page

--- frontend/src/landing_page/pricing/Brokerage.jsx
+++ frontend/src/landing_page/pricing/Brokerage.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 
 export default function Brokerage() {
+  let styles={width:"70%"}
   return (
-    <div>Brokerage</div>
+    <div className="container text-center">
+      <div className="row">
+        <div className="col p-5">
+          <img src="images/pricingEquity.svg" alt="Free equity delivery" style={styles}/>
+          <h3>Free equity delivery</h3>
+          <p className='text-muted'>All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
+        </div>
+        <div className="col p-5">
+          <img src="images/pricing20.svg" alt="Intraday and F&O trades"  style={styles}/>
+          <h3>Intraday and F&O trades</h3>
+          <p className='text-muted'>Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p>
+        </div>
+        <div className="col p-5">
+          <img src="images/pricingMF.svg" alt="Free direct MF" style={styles} />
+          <h3>Free direct MF</h3>
+          <p className='text-muted'>All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
+        </div>
+      </div>
+    </div>
   )
 }
--- frontend/src/landing_page/pricing/Hero.jsx
+++ frontend/src/landing_page/pricing/Hero.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='container'>
-        
+    <div className='container text-center p-5'>
+        <h2 className='mt-5'>Charges</h2>
+        <p className='fs-5 text-muted'>List of all charges and taxes</p>
     </div>
   )
 }
