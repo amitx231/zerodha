86aa032485c1f030823788c0fa5a3f01e1ac5cb7
Amit Chauhan <amitchauhan822006@gmail.com>
2026-08-13 02:50:05 +0530

    Connect MongoDB Atlas

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,34 @@
+if(process.env.NODE_ENV!="production")
+{
+    require("dotenv").config();
+}
+
+const dns = require("dns");
+
+dns.setServers([
+    "8.8.8.8",
+    "8.8.4.4"
+]);
+
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+// const dbUrl="mongodb://127.0.0.1:27017/zerodha";
+const dbUrl=process.env.ATLAS_URI;
+main()
+.then((res)=>{
+    console.log("MongoDB connected successfully!");
+})
+.catch((err)=>{
+    console.log("MongoDB connection error:",err);
+})
+
+
+async function main()
+{
+    await mongoose.connect(dbUrl);
+}
 
 app.get('/',(req,res)=>{
     res.send("App is working !");
